c0d9f7abc5d1b47bf15e035b8f1ca2937b72c890
Kulkarni, Sankalpa <Sankalpa_Kulkarni@intuit.com>
2018-09-28 15:54:20 -0700

    initial commit

--- photo-gallery/src/index.js
+++ photo-gallery/src/index.js
@@ -1,6 +1,10 @@
+import React from "react";
 import ReactDOM from "react-dom";
 import MessageComponent from "./MessageComponent";
 
 
+const Index = () => {
+    return (<div>Hello React!</div>);
+};
 
 ReactDOM.render(<MessageComponent />, document.getElementById('content'));
--- webpack-exercise/webpack.config.js
+++ webpack-exercise/webpack.config.js
@@ -35,9 +35,6 @@ module.exports = {
             }
         ]
     },
-    // externals: {
-    //     'react': 'React'
-    // },
     resolve: {
         extensions: ['.js', '.jsx']
     }
